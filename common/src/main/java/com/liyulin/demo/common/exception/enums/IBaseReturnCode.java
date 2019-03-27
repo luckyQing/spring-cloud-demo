08607333ccc31f06b1fd6c36f921e83c845febd3
luckyQing <1634753825@qq.com>
2019-03-27 23:29:20 +0800

    修改注释

--- common/src/main/java/com/liyulin/demo/common/exception/enums/IBaseReturnCode.java
+++ common/src/main/java/com/liyulin/demo/common/exception/enums/IBaseReturnCode.java
@@ -1,5 +1,19 @@
 package com.liyulin.demo.common.exception.enums;
 
+/**
+ * 状态码格式接口类：XXXXXX（服务模块编码|类型）
+ * 
+ * <ul>
+ * <li>XXX1XX：信息类
+ * <li>XXX2XX：操作成功
+ * <li>XXX3XX：重定向
+ * <li>XXX4XX：客户端错误
+ * <li>XXX5XX：服务器错误
+ * </ul>
+ *
+ * @author liyulin
+ * @date 2019年3月27日下午11:24:10
+ */
 public interface IBaseReturnCode {
 
 	String getCode();
--- common/src/main/java/com/liyulin/demo/common/exception/enums/ReturnCodeEnum.java
+++ common/src/main/java/com/liyulin/demo/common/exception/enums/ReturnCodeEnum.java
@@ -5,18 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
- * 状态码格式：XXXXXX（服务模块编码|类型）
+ * 通用状态码
  * 
- * <ul>
- * <li>XXX1XX：信息类
- * <li>XXX2XX：操作成功
- * <li>XXX3XX：重定向
- * <li>XXX4XX：客户端错误
- * <li>XXX5XX：服务器错误
- * </ul>
- *
  * @author liyulin
  * @date 2019年3月27日下午10:50:41
+ * @see IBaseReturnCode
  */
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
