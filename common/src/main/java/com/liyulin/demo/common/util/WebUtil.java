cdf27cf1a563d442b3f5432ef9c83db49020d809
luckyQing <1634753825@qq.com>
2019-04-04 17:18:01 +0800

    添加工具类

--- common/src/main/java/com/liyulin/demo/common/util/WebUtil.java
+++ common/src/main/java/com/liyulin/demo/common/util/WebUtil.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.URL;
+import java.net.UnknownHostException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -23,83 +24,93 @@ import lombok.extern.slf4j.Slf4j;
 public class WebUtil {
 
 	/**
-     * 获取请求的ip地址
-     *
-     * @param request http请求
-     * @return ip地址
-     */
-    public static String getRealIP(HttpServletRequest request) {
-        String ip = request.getHeader("x-forwarded-for");
-        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("Proxy-Client-IP");
-        }
-        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
-        }
-        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("http_client_ip");
-        }
-        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-        }
+	 * 获取本机ip地址
+	 * 
+	 * @return
+	 * @throws UnknownHostException
+	 */
+	public static String getLocalIP() throws UnknownHostException {
+		return InetAddress.getLocalHost().getHostAddress();
+	}
 
-        // 如果是多级代理，那么取第一个ip为客户ip
-        if (ip != null && ip.contains(",")) {
-            ip = ip.substring(ip.lastIndexOf(",") + 1, ip.length()).trim();
-        }
-        return ip;
-    }
+	/**
+	 * 获取请求的ip地址
+	 *
+	 * @param request http请求
+	 * @return ip地址
+	 */
+	public static String getRealIP(HttpServletRequest request) {
+		String ip = request.getHeader("x-forwarded-for");
+		if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+		if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("http_client_ip");
+		}
+		if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+		}
 
-    /**
-     * ip地址是否可以ping通
-     *
-     * @param ipAddress ip地址
-     * @param timeout   超时，单位毫秒
-     * @return true | false
-     */
-    public static boolean ipCanPing(String ipAddress, int timeout) {
-        if (StringUtils.isEmpty(ipAddress)) {
-            return false;
-        }
+		// 如果是多级代理，那么取第一个ip为客户ip
+		if (ip != null && ip.contains(",")) {
+			ip = ip.substring(ip.lastIndexOf(",") + 1, ip.length()).trim();
+		}
+		return ip;
+	}
 
-        boolean canPing = false;
-        try {
-            canPing = InetAddress.getByName(ipAddress).isReachable(timeout);
-        } catch (IOException e) {
-            log.error(e.getMessage(), e);
-        }
-        return canPing;
-    }
+	/**
+	 * ip地址是否可以ping通
+	 *
+	 * @param ipAddress ip地址
+	 * @param timeout   超时，单位毫秒
+	 * @return true | false
+	 */
+	public static boolean ipCanPing(String ipAddress, int timeout) {
+		if (StringUtils.isEmpty(ipAddress)) {
+			return false;
+		}
 
-    /**
-     * 链接是否可以被请求
-     *
-     * @param url     地址
-     * @param timeout 超时，单位毫秒
-     * @return true | false
-     */
-    public static boolean AddrCanConnect(String url, int timeout) {
-        if (StringUtils.isEmpty(url)) {
-            return false;
-        }
+		boolean canPing = false;
+		try {
+			canPing = InetAddress.getByName(ipAddress).isReachable(timeout);
+		} catch (IOException e) {
+			log.error(e.getMessage(), e);
+		}
+		return canPing;
+	}
 
-        boolean canConnect = false;
-        try {
-            URL urlConnection = new URL(url);
+	/**
+	 * 链接是否可以被请求
+	 *
+	 * @param url     地址
+	 * @param timeout 超时，单位毫秒
+	 * @return true | false
+	 */
+	public static boolean AddrCanConnect(String url, int timeout) {
+		if (StringUtils.isEmpty(url)) {
+			return false;
+		}
 
-            HttpURLConnection connection = (HttpURLConnection) urlConnection.openConnection();
-            connection.setUseCaches(false);
-            connection.setConnectTimeout(timeout);
-            int state = connection.getResponseCode();
-            canConnect = (state == HttpURLConnection.HTTP_OK);
-        } catch (Exception e) {
-        	log.error(e.getMessage(), e);
-        }
+		boolean canConnect = false;
+		try {
+			URL urlConnection = new URL(url);
 
-        return canConnect;
-    }
-	
+			HttpURLConnection connection = (HttpURLConnection) urlConnection.openConnection();
+			connection.setUseCaches(false);
+			connection.setConnectTimeout(timeout);
+			int state = connection.getResponseCode();
+			canConnect = (state == HttpURLConnection.HTTP_OK);
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
+
+		return canConnect;
+	}
+
 }
