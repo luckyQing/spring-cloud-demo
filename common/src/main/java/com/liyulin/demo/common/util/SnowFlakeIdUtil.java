d2c520286e2302cb4a04546889c579773ddafa1e
luckyQing <1634753825@qq.com>
2019-04-05 23:59:38 +0800

    修改id生成器

--- common/src/main/java/com/liyulin/demo/common/util/SnowFlakeIdUtil.java
+++ common/src/main/java/com/liyulin/demo/common/util/SnowFlakeIdUtil.java
@@ -1,43 +1,43 @@
 package com.liyulin.demo.common.util;
 
+import java.net.UnknownHostException;
+
 /**
- * Twitter_Snowflake<br>
- * SnowFlake的结构如下(每部分用-分开):<br>
- * 0 - 0000000000 0000000000 0000000000 0000000000 0 - 00000 - 00000 -
- * 000000000000 <br>
- * 1位标识，由于long基本类型在Java中是带符号的，最高位是符号位，正数是0，负数是1，所以id一般是正数，最高位是0<br>
- * 41位时间截(毫秒级)，注意，41位时间截不是存储当前时间的时间截，而是存储时间截的差值（当前时间截 - 开始时间截)
+ * Twitter_Snowflake
+ * 
+ * <h2>SnowFlake的结构如下(每部分用-分开):</h2>
+ * <p>0 - 0000000000 0000000000 0000000000 0000000000 0 - 00000 - 00000 - 000000000000
+ * <ul>
+ * <li>1位标识，由于long基本类型在Java中是带符号的，最高位是符号位，正数是0，负数是1，所以id一般是正数，最高位是0
+ * <li>41位时间截(毫秒级)，注意，41位时间截不是存储当前时间的时间截，而是存储时间截的差值（当前时间截 - 开始时间截)
  * 得到的值），这里的的开始时间截，一般是我们的id生成器开始使用的时间，由我们程序来指定的（如下下面程序IdWorker类的startTime属性）。
- * 41位的时间截，可以使用69年，年T = (1L << 41) / (1000L * 60 * 60 * 24 * 365) = 69<br>
- * 10位的数据机器位，可以部署在1024个节点，包括5位datacenterId和5位workerId<br>
- * 12位序列，毫秒内的计数，12位的计数顺序号支持每个节点每毫秒(同一机器，同一时间截)产生4096个ID序号<br>
- * 加起来刚好64位，为一个Long型。<br>
- * SnowFlake的优点是，整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞(由数据中心ID和机器ID作区分)，并且效率较高，
- * 经测试，SnowFlake每秒能够产生26万ID左右。
+ * 41位的时间截，可以使用69年，年T = (1L << 41) / (1000L * 60 * 60 * 24 * 365) = 69
+ * <li>10位的数据机器位，可以部署在1024个节点，包括5位datacenterId和5位workerId
+ * <li>12位序列，毫秒内的计数，12位的计数顺序号支持每个节点每毫秒(同一机器，同一时间截)产生4096个ID序号
+ * </ul>
+ * <p>加起来刚好64位，为一个Long型。
+ * 
+ * <h2>优缺点</h2>
+ * <ul>
+ * <li>优点：整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞(由数据中心ID和机器ID作区分)，并且效率较高。
+ * <li>缺点：对系统时间的依赖性非常强，需关闭ntp的时间同步功能。
+ * </ul>
  */
-public class SnowFlakeId {
-	/**
-	 * 起始的时间戳
-	 */
+public final class SnowFlakeIdUtil {
+	/** 起始的时间戳 */
 	private final static long START_STMP = 1480166465631L;
 
-	/**
-	 * 每一部分占用的位数
-	 */
+	/** 每一部分占用的位数 */
 	private final static long SEQUENCE_BIT = 12; // 序列号占用的位数
 	private final static long MACHINE_BIT = 5; // 机器标识占用的位数
 	private final static long DATACENTER_BIT = 5;// 数据中心占用的位数
 
-	/**
-	 * 每一部分的最大值
-	 */
+	/** 每一部分的最大值 */
 	private final static long MAX_DATACENTER_NUM = -1L ^ (-1L << DATACENTER_BIT);
 	private final static long MAX_MACHINE_NUM = -1L ^ (-1L << MACHINE_BIT);
 	private final static long MAX_SEQUENCE = -1L ^ (-1L << SEQUENCE_BIT);
 
-	/**
-	 * 每一部分向左的位移
-	 */
+	/** 每一部分向左的位移 */
 	private final static long MACHINE_LEFT = SEQUENCE_BIT;
 	private final static long DATACENTER_LEFT = SEQUENCE_BIT + MACHINE_BIT;
 	private final static long TIMESTMP_LEFT = DATACENTER_LEFT + DATACENTER_BIT;
@@ -48,13 +48,32 @@ public class SnowFlakeId {
 	 * 中心标识ID和机器进程标识ID作为联合键全局唯一，这样才能保证集群生成的ID都是唯一的。
 	 */
 	// TODO:在分布式集群环境下，可以用服务名作为唯一编码，在配置中心配置。
-	private long datacenterId; // 数据中心
-	private long machineId; // 机器标识
+	/** 数据中心 */
+	private long datacenterId;
+	/** 机器标识 */
+	private long machineId;
+	/** 序列号 */
+	private long sequence = 0L;
+	/** 上一次时间戳 */
+	private volatile long lastStmp = -1L;
+	
+	private static SnowFlakeIdUtil flowIdWorker = new SnowFlakeIdUtil();
 
-	private long sequence = 0L; // 序列号
-	private volatile long lastStmp = -1L;// 上一次时间戳
+	public static SnowFlakeIdUtil getInstance() {
+		return flowIdWorker;
+	}
+	
+	private long getDatacenterId() {
+		try {
+			String ip = WebUtil.getLocalIP().split(".")[3];
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
 
-	public SnowFlakeId(long datacenterId, long machineId) {
+	private SnowFlakeIdUtil(/*long datacenterId, long machineId*/) {
+		
 		if (datacenterId > MAX_DATACENTER_NUM || datacenterId < 0) {
 			throw new IllegalArgumentException("datacenterId can't be greater than MAX_DATACENTER_NUM or less than 0");
 		}
@@ -109,7 +128,7 @@ public class SnowFlakeId {
 	}
 
 	public static void main(String[] args) {
-		
+
 	}
-	
+
 }
