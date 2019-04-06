b0bb4ceb89ac2ebac7a0ff712ce421cb71746f87
luckyQing <1634753825@qq.com>
2019-04-06 19:56:32 +0800

    完善

--- common/src/main/java/com/liyulin/demo/common/business/dto/RespHead.java
+++ common/src/main/java/com/liyulin/demo/common/business/dto/RespHead.java
@@ -22,15 +22,15 @@ public class RespHead extends BaseDto {
 	@Builder.Default
 	private String transactionId = TransactionIdUtil.getInstance().nextId();
 
-	@ApiModelProperty(value = "响应状态码")
+	@ApiModelProperty(value = "响应状态码", example = "100500")
 	private String code;
 
-	@ApiModelProperty(value = "提示信息")
+	@ApiModelProperty(value = "提示信息", example = "服务器异常")
 	private String msg;
 
-	@ApiModelProperty(value = "错误详情")
+	@ApiModelProperty(value = "错误详情", example = "错误详细信息")
 	private String error;
-	
+
 	@ApiModelProperty(value = "响应时间戳", example = "1554551377629")
 	@Builder.Default
 	private long timestamp = System.currentTimeMillis();
@@ -39,7 +39,7 @@ public class RespHead extends BaseDto {
 		this.code = returnCode.getCode();
 		this.msg = returnCode.getMsg();
 	}
-	
+
 	public RespHead(String code, String msg) {
 		this.code = code;
 		this.msg = msg;
