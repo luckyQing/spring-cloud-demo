a4b7941ec4ece724cfdcb7f138654bfb7bdd5f06
luckyQing <1634753825@qq.com>
2019-04-17 17:26:00 +0800

    完善

---
+++ common/src/main/java/com/liyulin/demo/common/test/FiegnMockAgent.java
@@ -0,0 +1,10 @@
+package com.liyulin.demo.common.test;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnNotWebApplication;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//@ConditionalOnNotWebApplication
+//public class FiegnMockAgent {
+//
+//}
