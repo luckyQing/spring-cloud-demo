b3749084bcd2fb5eaef0cb5751c32a49aa908d7f
luckyQing <1634753825@qq.com>
2019-04-26 00:23:00 +0800

    完善

--- common/src/main/java/com/liyulin/demo/common/support/UniqueBeanNameGenerator.java
+++ common/src/main/java/com/liyulin/demo/common/support/UniqueBeanNameGenerator.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.annotation.AnnotationBeanNameGenerator;
 import org.springframework.util.StringUtils;
 
-import com.liyulin.demo.common.constants.CommonConstants;
-
 /**
  * 生成bean名称的规则
  *
@@ -29,22 +27,13 @@ public class UniqueBeanNameGenerator extends AnnotationBeanNameGenerator {
 
 		// 默认的bean名称（不含package，首字母小写）
 		String defaultClassName = super.buildDefaultBeanName(definition);
-		// bean类名（package+className）
-		String beanClassName = definition.getBeanClassName();
-		System.err.println(beanClassName);
-		if (beanClassName.startsWith(CommonConstants.BASE_PACAKGE)) {
-			// 如果是非第三方的类
-			// 如果该bean名称不存在容器中，则按照className的规则生成；否则按照“package+className”的规则存在。
-			BeanFactory beanFactory = (BeanFactory) registry;
-			if (beanFactory.containsBean(defaultClassName)) {
-				// 如果是非第三方的类，则bean名称直接返回“包名.类名”
-				return beanClassName;
-			} else {
-				return defaultClassName;
-			}
+		// 如果该bean名称不存在容器中，则按照className的规则生成；否则按照“package+className”的规则存在。
+		BeanFactory beanFactory = (BeanFactory) registry;
+		if (beanFactory.containsBean(defaultClassName)) {
+			return definition.getBeanClassName();
+		} else {
+			return defaultClassName;
 		}
-
-		return defaultClassName;
 	}
 
 }
