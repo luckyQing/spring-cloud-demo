727c69763654bfcb475dc05bb2eaaa5ca601d161
li xian chun <2629469408@qq.com>
2019-05-01 11:33:38 +0800

    配置service集群

--- demo-eureka-module/demo-eureka-nodeA/src/main/java/com/liyulin/demo/Application.java
+++ demo-eureka-module/demo-eureka-nodeA/src/main/java/com/liyulin/demo/Application.java
@@ -2,14 +2,14 @@ package com.liyulin.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableEurekaServer
 public class Application {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
 }
