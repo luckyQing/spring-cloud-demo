c7e073b55a265555e69b3fd1fcba977fe8020571
luckyQing <1634753825@qq.com>
2019-05-20 01:13:06 +0800

    完善

---
+++ common/src/main/java/com/liyulin/demo/common/web/swagger/SwaggerParse.java
@@ -0,0 +1,10 @@
+package com.liyulin.demo.common.web.swagger;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class SwaggerParse {
+	
+	@Value("server.port")
+	private String port;
+
+}
--- demo-mall-module/demo-mall-product-service/src/main/java/com/liyulin/demo/mall/product/Application.java
+++ demo-mall-module/demo-mall-product-service/src/main/java/com/liyulin/demo/mall/product/Application.java
@@ -4,11 +4,16 @@ import org.springframework.boot.SpringApplication;
 
 import com.liyulin.demo.common.support.annotation.DefaultSmartSpringCloudApplication;
 
+//import io.swagger.models.Swagger;
+//import io.swagger.parser.SwaggerParser;
+
 @DefaultSmartSpringCloudApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+//		Swagger swagger = new SwaggerParser().read("http://localhost:20021/v2/api-docs?group=demoMallProductService");
+//		System.err.println(JSON.toJSONString(swagger));
 	}
 
 }
