742a735e591399158cc2b1e5c5400511c3c59ce1
luckyQing <1634753825@qq.com>
2019-05-28 19:43:07 +0800

    优化

--- demo-mapper-common/src/main/java/com/liyulin/demo/mybatis/autoconfigure/MultipleDataSourceAutoConfiguration.java
+++ demo-mapper-common/src/main/java/com/liyulin/demo/mybatis/autoconfigure/MultipleDataSourceAutoConfiguration.java
@@ -1,8 +1,13 @@
 package com.liyulin.demo.mybatis.autoconfigure;
 
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.liyulin.demo.mybatis.properties.MultipleDatasourceProperties;
@@ -16,7 +21,27 @@ import com.liyulin.demo.mybatis.properties.MultipleDatasourceProperties;
 @Configuration
 @EnableConfigurationProperties(MultipleDatasourceProperties.class)
 @EnableTransactionManagement
-@Import({MultipleDataSourceInitializationConfiguration.class })
+@Import({ MultipleDataSourceInitializerInvoker.class, MultipleDataSourceAutoConfiguration.Registrar.class })
 public class MultipleDataSourceAutoConfiguration {
-	
+
+	static class Registrar implements ImportBeanDefinitionRegistrar {
+
+		private static final String BEAN_NAME = "multipleDataSourceInitializerPostProcessor";
+
+		@Override
+		public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata,
+				BeanDefinitionRegistry registry) {
+			if (!registry.containsBeanDefinition(BEAN_NAME)) {
+				GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+				beanDefinition.setBeanClass(MultipleDataSourceInitializerPostProcessor.class);
+				beanDefinition.setRole(BeanDefinition.ROLE_INFRASTRUCTURE);
+				// We don't need this one to be post processed otherwise it can cause a
+				// cascade of bean instantiation that we would rather avoid.
+				beanDefinition.setSynthetic(true);
+				registry.registerBeanDefinition(BEAN_NAME, beanDefinition);
+			}
+		}
+
+	}
+
 }
