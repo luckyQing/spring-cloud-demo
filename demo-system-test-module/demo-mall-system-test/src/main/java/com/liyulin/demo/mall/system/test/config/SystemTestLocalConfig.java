f6f9750ad98cd637f6fbd034aeaa93badc35ff38
luckyQing <1634753825@qq.com>
2019-07-11 02:10:59 +0800

    完善

--- demo-system-test-module/demo-mall-system-test/src/main/java/com/liyulin/demo/mall/system/test/config/SystemTestLocalConfig.java
+++ demo-system-test-module/demo-mall-system-test/src/main/java/com/liyulin/demo/mall/system/test/config/SystemTestLocalConfig.java
@@ -2,7 +2,7 @@ package com.liyulin.demo.mall.system.test.config;
 
 public class SystemTestLocalConfig extends AbstractSystemTestConfig {
 
-	private static final boolean MERGE_MODULE = false;
+	private static final boolean MERGE_MODULE = true;
 	private static final String MERGE_MALL_BASE_URL = "http://localhost:30001/";
 
 	@Override
